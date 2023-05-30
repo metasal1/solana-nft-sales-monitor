cd6ce8614fcea324402934fc624c7a7599f37b74
Salim Karim <imsjk8@gmail.com>
2023-05-30 13:03:11 +1000

    mongo fixed

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -15,7 +15,14 @@ const client = new MongoClient(uri, {
 export const saveToMongo = async (data, db, table) => {
     try {
         await client.connect();
-        const req = await client.db(db).collection(table).insertOne(data)
+
+        const { date, salesPrice, signature } = data;
+        const { name: exchangeName } = data.exchange;
+        const { name: nftName } = data.metadata.offChain;
+
+        const item = { date: new Date(), saleDate: date.toLocaleString(), nftName, salesPrice, exchangeName, signature };
+
+        const req = await client.db(db).collection(table).insertOne(item)
         console.log(req.acknowledged, req.insertedId, new Date())
     } finally {
         // await client.close();
